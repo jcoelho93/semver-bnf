96bd89f1e1c028fcfbe975d7741c989e3d7b0ed0
Coelho Jose (CC-AD/EPE-Bp) <bosch.coelho@extaccount.com>
2020-07-28 13:41:53 +0200

    Add setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='semver-bnf',
+    version='0.0.1',
+    description="A semantic version helper that uses Backus-Naur form",
+    url="https://github.com/jcoelho93/semver-bnf",
+    author="José Coelho",
+    license="MIT",
+    packages=['semver_bnf'],
+    zip_safe=False
+)
